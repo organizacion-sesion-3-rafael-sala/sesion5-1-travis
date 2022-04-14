21fce4f333ce9cf77dec75f4eb3385afabbcf979
RafaSalaEsteve <102507108+RafaSalaEsteve@users.noreply.github.com>
2022-04-14 12:08:23 +0200

    Create EvenTest.java

---
+++ tests/EvenTest.java
@@ -0,0 +1,26 @@
+
+public class EvenTest {
+
+    public static boolean testisEven(){
+    
+      boolean resultado
+        
+       if( Even.isEven(10)){
+        if( Even.isEven(8){
+          if( Even.isEven(42){
+            if( !Even.isEven(9){
+              if( !Even.isEven(7){
+                if( !Even.isEven(3){
+                  if( !Even.isEven(1){
+                    return true;
+                  }
+                }
+              }
+            }
+          }
+        }
+       
+       }
+    return false;
+    }
+}
